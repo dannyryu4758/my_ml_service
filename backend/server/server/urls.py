a7928720d6c69fe1159b22118bc6f1fa6de90660
RyuHogun <loveme6230@naver.com>
2022-08-18 14:25:19 +0900

    setup django project

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -1,7 +1,7 @@
 """server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-urlpatterns += endpoints_urlpatterns
