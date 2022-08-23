7c6dc9b39df061114c1d388c6517f5314a118199
RyuHogun <loveme6230@naver.com>
2022-08-23 16:05:33 +0900

    add predict view

--- backend/server/apps/endpoints/views.py
+++ backend/server/apps/endpoints/views.py
@@ -1,9 +1,15 @@
 # backend/server/apps/endpoints/views.py file
+from ensurepip import version
+import json
+from numpy.random import rand
+
 from django.db import transaction
 
 from rest_framework.exceptions import APIException
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework import views, status
+from rest_framework.response import Response
 
 from apps.endpoints.models import Endpoint
 from apps.endpoints.serializers import EndpointSerializer
@@ -16,6 +22,10 @@ from apps.endpoints.serializers import MLAlgorithmStatusSerializer
 
 from apps.endpoints.models import MLRequest
 from apps.endpoints.serializers import MLRequestSerializer
+
+from apps.ml.registry import MLRegistry
+
+from server.wsgi import registry
 
 class EndpointViewSet(
     mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
@@ -51,9 +61,6 @@ class MLAlgorithmStatusViewSet(
                 instance = serializer.save(active=True)
                 # set active=False for other statuses
                 deactivate_other_statuses(instance)
-
-
-
         except Exception as e:
             raise APIException(str(e))
 
@@ -63,3 +70,51 @@ class MLRequestViewSet(
 ):
     serializer_class = MLRequestSerializer
     queryset = MLRequest.objects.all()
+    
+
+class PredictView(views.APIView) :
+    def post(self, request, endpoint_name, format=None):
+        algorithm_status = self.request.query_params.get("status", "production")
+        algorithm_version = self.request.query_params.get("version")
+        algs = MLAlgorithm.objects.filter(parent_endpoint__name=endpoint_name, status__status=algorithm_status, status__active=True)
+        
+        if algorithm_version is not None :
+            algs = algs.filter(version=algorithm_version)
+        
+        if len(algs) == 0 :
+            return Response(
+                {
+                    "status" : "Error",
+                    "message" : "ML alogrithm is not available"
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        if len(algs) != 1 and algorithm_status != "ab_testing":
+            return Response(
+                {
+                    "status" : "Error",
+                    "message" : "ML alogrithm selection is ambiguous. Please specify alogrithm version."    
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        alg_index = 0
+        if algorithm_status == "ab_testing" :
+            alg_index = 0  if rand() < 0.5 else 1
+            
+        algorithm_object = registry.endpoints[algs[alg_index].id]
+        prediction = algorithm_object.compute_prediction(request.data)
+        
+        label = prediction['label'] if "label" in prediction else "error"
+        ml_request = MLRequest(
+            input_data=json.dumps(request.data),
+            full_response=prediction,
+            response=label,
+            feedback="",
+            parent_mlalgorithm=algs[alg_index],
+        )
+        ml_request.save()
+        prediction["request_id"]=ml_request.id
+        
+        return Response(prediction)
